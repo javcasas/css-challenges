ee2550b728d68bd4507bbe4b38bef5b5165f94f1
Javier Casas <javcasas@gmail.com>
2017-09-20 17:11:45 -0500

    Proper scss support

--- css-challenges/src/components/menus/index.vue
+++ css-challenges/src/components/menus/index.vue
@@ -63,8 +63,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-@import './index.scss';
+<style lang="scss" scoped>
 
 nav {
   ul {
